1263a1235a26e1b37e56ecab5a41baa6c828e679
Tony Lutz <anthonyblutz@gmail.com>
2022-08-05 17:18:12 +0000

    deleting files

--- refactoringIAC/cognito.tf
+++ refactoringIAC/cognito.tf
@@ -71,32 +71,6 @@ resource "aws_cognito_user_pool_domain" "cms_acme_user_pool" {
 
  }
 
-
-# resource "aws_cognito_identity_pool" "cms_acme_user_pool" {
-#   identity_pool_name               = "Cognito-Pool"
-#   allow_unauthenticated_identities = false
-#   allow_classic_flow               = false
-
-#   cognito_identity_providers {
-#     client_id               = aws_cognito_user_pool_client.cms_acme_user_pool.id
-#     provider_name           = aws_cognito_user_pool.cms_acme_user_pool.endpoint
-#     server_side_token_check = false
-#   }
-
-#   tags = {
-#     Name = "ansong844-identity-pool"
-#   }
-# }
-
-# resource "aws_cognito_user_pool_ui_customization" "cms_acme_user_pool" {
-#   css          = ".label-customizable {font-weight: 400;}"
-#   image_file   = filebase64("acme-cms.png")
-#   user_pool_id = aws_cognito_user_pool_domain.cms_acme_user_pool.user_pool_id
-# }
-
-
-
-
 resource "aws_cognito_user_group" "EHR_maintainer" {
   name         = "EHR_maintainer"
   user_pool_id = aws_cognito_user_pool.cms_acme_user_pool.id
