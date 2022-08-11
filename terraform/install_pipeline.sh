de510b4bd5b91c1097be056a63827170efb6b7d5
Tony Lutz <anthonyblutz@gmail.com>
2022-08-11 21:11:06 +0000

    updates

--- terraform/install_pipeline.sh
+++ terraform/install_pipeline.sh
@@ -1,13 +1,12 @@
 sudo yum -y update
-sudo yum -y install yum-utils 
+sudo yum -y install yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
 sudo yum-config-manager --add-repo https://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo
-# sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
+
+sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
 
 sudo yum -y install terraform jq apache-maven java-17-amazon-corretto-devel
 sudo amazon-linux-extras install -y docker
-
-terraform -install-autocomplete
 
 sudo /usr/sbin/alternatives --set java /usr/lib/jvm/java-17-amazon-corretto.x86_64/bin/java
 sudo /usr/sbin/alternatives --set javac /usr/lib/jvm/java-17-amazon-corretto.x86_64/bin/javac
@@ -17,4 +16,5 @@ sudo systemctl enable docker
 sudo usermod -a -G docker ec2-user
 
 chmod +x seed_data.sh
+chmod +x cleanup.sh
 ./seed_data.sh
