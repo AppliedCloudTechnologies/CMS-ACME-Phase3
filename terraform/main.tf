b37cc38db1b60d0bc12940e586737d2549a122c2
Tony Lutz <anthonyblutz@gmail.com>
2022-08-12 03:48:58 +0000

    updated tags

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,6 +9,13 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
+
+    default_tags {
+    tags = {
+      Environment = "TechChallenge"
+      Owner       = "Winners"
+    }
+  }
 }
 
 data "aws_caller_identity" "current" {}
